00b25551c4be0b2ed92af4819d4692e1fe4e2899
Gamze Ercan <gamzeipek06@gmail.com>
2025-11-14 14:59:42 +0100

    first commit

---
+++ src/pages/firstPage.tsx
@@ -0,0 +1,10 @@
+const firstPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the First Page</h1>
+      <p>This is the content of the first page.</p>
+    </div>
+  );
+};
+
+export default firstPage;
